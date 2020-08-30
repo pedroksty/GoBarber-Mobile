b9fb2e07d58162e03d4162543a00928915b6741e
pedroksty <pedrotrab2132@gmail.com>
2020-08-29 22:05:20 -0300

    Connect ProvidersList component in Dashboard to API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
@@ -9,15 +9,34 @@ import { Container,
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList
  } from './styles';
 
+import api from '../../services/api';
+
+export interface Provider {
+  id: string
+  name: string
+  avatar_url: string
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([])
+
   const { signOut, user } = useAuth();
   const {navigate} = useNavigation()
 
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data)
+    })
+  }, [])
+
   const navigateToProfile = useCallback(() => {
-    navigate('Profile')
+    signOut()
   }, [navigate])
+
+
 
   return (
     <Container>
@@ -31,6 +50,14 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{uri: user.avatar_url ? user.avatar_url : 'https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png'}} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList   
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        renderItem={({item}) => (
+          <UserName>{item.name}</UserName>
+        )}
+      />
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { FlatList } from 'react-native'
+import { Provider } from './index'
 
 export const Container = styled.View`
   flex:1;
@@ -39,5 +41,10 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
 `
+export const ProvidersList = styled(
+  FlatList as new () => FlatList<Provider>
+)`
 
+
+`
 
