847417c8f8a6d00150d5d0fe472a310315e43350
pedroksty <pedrotrab2132@gmail.com>
2020-08-29 21:44:11 -0300

    Create Dashboard Header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,16 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string
+  avatar_url: string
+  name: string
+  email: string
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCrendentials {
@@ -19,7 +26,7 @@ interface SignInCrendentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(crendentials: SignInCrendentials): Promise<void>;
   signOut(): void;
   loading: boolean;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
 
+import { Container,
+  Header,
+  HeaderTitle, 
+  UserName,
+  ProfileButton,
+  UserAvatar,
+ } from './styles';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const {navigate} = useNavigation()
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile')
+  }, [navigate])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile} >
+          <UserAvatar source={{uri: user.avatar_url ? user.avatar_url : 'https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png'}} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
